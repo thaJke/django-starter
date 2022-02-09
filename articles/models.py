c53678f1f622a93859a2ac5c1ea54333917f1d14
thaJke <tjaynyirenda@gmail.com>
2022-02-09 14:40:50 +0200

    added new templates path made a new model in models.py

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,12 @@
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Articles(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE,)
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,10 @@
+import re
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def articles(request):
+
+    return render(request, 'articles/index.html')
+    
